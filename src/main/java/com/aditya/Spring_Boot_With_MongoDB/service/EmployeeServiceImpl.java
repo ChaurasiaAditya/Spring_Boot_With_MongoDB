1293c4a155fec3d6adb874c380815678c46cd241
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-24 10:28:19 +0530

    implemented all the methods from EmployeeService interface

--- src/main/java/com/aditya/Spring_Boot_With_MongoDB/service/EmployeeServiceImpl.java
+++ src/main/java/com/aditya/Spring_Boot_With_MongoDB/service/EmployeeServiceImpl.java
@@ -7,5 +7,33 @@
  */
 package com.aditya.Spring_Boot_With_MongoDB.service;
 
-public class EmployeeServiceImpl {
+import com.aditya.Spring_Boot_With_MongoDB.domain.Employee;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService{
+	@Override
+	public Employee add(Employee employee) {
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAll() {
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllByName(String name) {
+		return null;
+	}
+
+	@Override
+	public Employee getEmployeeByEmail(String email) {
+		return null;
+	}
+
+	@Override
+	public List<Employee> getEmployeeByDepartment(String departmentName) {
+		return null;
+	}
 }
