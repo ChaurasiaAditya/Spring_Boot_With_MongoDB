395b3d13d390a0a0b2f150ad37eacba941bef67e
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-24 10:51:40 +0530

    created Attribute and generated constructor

--- src/main/java/com/aditya/Spring_Boot_With_MongoDB/controller/EmployeeController.java
+++ src/main/java/com/aditya/Spring_Boot_With_MongoDB/controller/EmployeeController.java
@@ -7,5 +7,17 @@
  */
 package com.aditya.Spring_Boot_With_MongoDB.controller;
 
+import com.aditya.Spring_Boot_With_MongoDB.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class EmployeeController {
+
+	private final EmployeeService employeeService;
+
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
 }
