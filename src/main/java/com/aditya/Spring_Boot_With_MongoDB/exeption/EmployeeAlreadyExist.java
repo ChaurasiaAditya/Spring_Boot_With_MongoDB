3998890f1e194523789f7e3c0a1eb1d630ba0cca
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-24 14:57:06 +0530

    created EmployeeAlreadyExist.java class

---
+++ src/main/java/com/aditya/Spring_Boot_With_MongoDB/exeption/EmployeeAlreadyExist.java
@@ -0,0 +1,11 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 24-11-2022
+ * Created With: IntelliJ IDEA Ultimate
+ * Profile: github.com/ChaurasiaAditya
+ * Website: ChaurasiaAditya.in
+ */
+package com.aditya.Spring_Boot_With_MongoDB.exeption;
+
+public class EmployeeAlreadyExist {
+}
