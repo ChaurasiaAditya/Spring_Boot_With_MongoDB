87434f581c13b6daad7003e20d811106e94f8627
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-24 09:41:06 +0530

    created Employee.java class

---
+++ src/main/java/com/aditya/Spring_Boot_With_MongoDB/domain/Employee.java
@@ -0,0 +1,12 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 24-11-2022
+ * Created With: IntelliJ IDEA Ultimate
+ * Profile: github.com/ChaurasiaAditya
+ * Website: ChaurasiaAditya.in
+ */
+package com.aditya.Spring_Boot_With_MongoDB.domain;
+
+public class Employee {
+
+}
